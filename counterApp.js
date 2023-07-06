fa0dfde3400fdfc0bf64b8139b2b85131d8f011b
Fayyez <fayyez@gmail.com>
2023-07-06 15:18:46 +0500

    added alert box

--- counterApp.js
+++ counterApp.js
@@ -1,17 +1,45 @@
 let peopleCount = 0,
   countEl = document.getElementById("counter"),
   reset_p = document.getElementById("res-p");
+popup = document.getElementById("Popup");
+
+function sleepFor(seconds) {
+  seconds = seconds * 1000;
+  return new Promise((resolve) => {
+    setTimeout(resolve, seconds);
+  });
+}
+
+async function hideElement(element) {
+  await sleepFor(1.5);
+  popup.style.display = "none";
+}
+
+function showPopup(msg,color) {
+  popup.innerText = msg;
+  popup.style.backgroundColor = color
+  popup.style.display = "inline";
+  hideElement(popup);
+}
 
 function add() {
   peopleCount += 1;
   countEl.textContent = peopleCount;
 }
+
 function subtract() {
-  peopleCount -= 1;
-  countEl.textContent = peopleCount;
+  if (peopleCount == 0) {
+    showPopup("Number of people cannot be less thsn '0'", "red");
+  } else {
+    peopleCount -= 1;
+    countEl.textContent = peopleCount;
+  }
 }
 
 function reset() {
   let alpha = peopleCount + " - ";
   reset_p.textContent += alpha;
+  countEl.textContent = 0;
+  peopleCount = 0;
+  showPopup("The counter has been Reset!!", "green");
 }
